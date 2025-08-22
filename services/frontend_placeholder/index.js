c3f27ded122f7e496f8dde1654a8e97a23c88cb4
Cyl-at-cluster <cyl-at-cluster@users.noreply.github.com>
2025-08-22 16:58:20 +0200

    WIP #1

---
+++ services/frontend_placeholder/index.js
@@ -0,0 +1,3 @@
+// npm i fastify @fastify/static
+
+
---
+++ services/users/index.js
@@ -0,0 +1,6 @@
+// npm i fastify @fastify/static
+// npm i @fastify/cookie
+// npm i @fastify/jwt
+// npm i sqlite3
+// npm i prisma --save-dev
+
