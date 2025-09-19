463014fe4c4c1029498565edc33b6e6d628d9de1
jdhallen <jdhallen@jacknoisette.lan>
2025-09-19 13:16:19 +0000

    avatar

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -6,7 +6,7 @@ import Header from '../components/Header';
 function AuthLayout() {
   return (
     <div className="h-full w-full flex flex-col items-center justify-start
-      px-1 py-7 gap-32">
+      px-1 py-7 gap-15">
       <Header disabled={true}/>
       <Card maxWidth="447px" maxHeight="559px" className="flex flex-col justify-center items-center p-2.5">
         <Outlet />
