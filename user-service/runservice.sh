907fc5df6b83d4f4d2e87054f8d21c00d8d66ce8
thryndir <neon.galgamergal@gmail.com>
2025-09-22 16:50:03 +0200

    update back

---
+++ user-service/runservice.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+clear
+export DATABASE_URL="file:./users_db.db"
+export AUTH_SERVICE_URL="http://localhost:3001"
+export API_PASSPHRASE="huhuhuhahahihi"
+export JWT_SECRET="your_jwt_secret_key_change_this_in_production"
+export PORT="3002"
+node index.js
