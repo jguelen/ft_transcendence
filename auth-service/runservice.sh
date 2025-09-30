122a41a599eec36c2f800355befefd387fa8434e
rvandepu <rvandepu@student.42lehavre.fr>
2025-09-30 14:20:49 +0200

    SSL to secrets, sample env

--- auth-service/runservice.sh
+++ auth-service/runservice.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 clear
-export GITHUB_OAUTH_ID="id"
-export GITHUB_OAUTH_SECRET="secret"
-export API_PASSPHRASE="huhuhuhahahihi"
 export USER_SERVICE_URL="http://localhost:3002"
-export JWT_SECRET="your_jwt_secret_key_change_this_in_production"
 export PORT="3001"
+. ../.env
+export GITHUB_OAUTH_ID
+export GITHUB_OAUTH_SECRET
+export API_PASSPHRASE
+export JWT_SECRET
 node index.js
--- user-service/runservice.sh
+++ user-service/runservice.sh
@@ -3,7 +3,8 @@
 clear
 export DATABASE_URL="file:./users_db.db"
 export AUTH_SERVICE_URL="http://localhost:3001"
-export API_PASSPHRASE="huhuhuhahahihi"
-export JWT_SECRET="your_jwt_secret_key_change_this_in_production"
 export PORT="3002"
+. ../.env
+export API_PASSPHRASE
+export JWT_SECRET
 node index.js
